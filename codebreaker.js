3c8fccf88c0a4723668ce6363cd0b7b07111cd5b
Praridz <rios.371@hotmail.com>
2020-05-16 12:01:46 -0500

    dojo codebreaker tdd

---
+++ codebreaker.js
@@ -0,0 +1,32 @@
+function codebreaker (a){
+    let numerosecreto  = generateNumber();
+
+    const vectorA = [...a];
+    const vectorB = [...numerosecreto];
+    let respuesta = '';
+
+    for(var i = 0;i<vectorA.length;i++){
+        for(var j = 0;j<vectorB.length;j++){
+           if(!vectorA[i].localeCompare(vectorB[j])){
+               if(i===j){
+                    respuesta = respuesta.concat('x');
+               }else{
+                    respuesta = respuesta.concat('-');
+               }
+           }
+         }
+    }
+    return respuesta;
+}
+
+function generateRandom(){
+    const random = Math.floor(Math.random()*1000);
+    const randomS = Number.toString(random);
+    return randomS; 
+}
+
+function generateNumber(){
+    return '2169';
+}
+
+module.exports.codebreaker = codebreaker;
---
+++ codebreaker.test.js
@@ -0,0 +1,51 @@
+const cb = require('./codebreaker'); // Arrange
+
+describe('codebreaker', () => {
+    test('test 8888 to equal 2169', () => {
+      var value = cb.codebreaker('8888'); //Act
+      expect(value).toBe(''); //Assert
+    });
+    test('test 2169 to equal 2169', () => {
+        var value = cb.codebreaker('2169'); //Act
+        expect(value).toBe('xxxx'); //Assert
+      });
+      //posibles casos de 1 x
+    test('test 2075 to equal 2169', () => {
+        var value = cb.codebreaker('2075'); //Act
+        expect(value).toBe('x'); //Assert
+    });
+
+    test('test 0175 to equal 2169', () => {
+        var value = cb.codebreaker('0175'); //Act
+        expect(value).toBe('x'); //Assert
+    });
+    test('test 7065 to equal 2169', () => {
+        var value = cb.codebreaker('7065'); //Act
+        expect(value).toBe('x'); //Assert
+    });
+    test('test 5079 to equal 2169', () => {
+        var value = cb.codebreaker('5079'); //Act
+        expect(value).toBe('x'); //Assert
+    });
+
+    test('test 8165 to equal 2169', () => {
+        var value = cb.codebreaker('8165'); //Act
+        expect(value).toBe('xx'); //Assert
+    });
+
+    test('test 2869 to equal 2169', () => {
+        var value = cb.codebreaker('2869'); //Act
+        expect(value).toBe('xxx'); //Assert
+    });
+
+    test('test 9612 to equal 2169', () => {
+        var value = cb.codebreaker('9612'); //Act
+        expect(value).toBe('----'); //Assert
+    });
+
+    test('test 2196 to equal 2169', () => {
+        var value = cb.codebreaker('2196'); //Act
+        expect(value).toBe('xx--'); //Assert
+    });
+
+})
